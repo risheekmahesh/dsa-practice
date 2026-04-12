76af7580656dadba6461898916821147621c20a6
Risheek Mahesh <risheek951@gmail.com>
2026-04-12 15:25:45 +0530

    Update palindrome.cpp

--- LeetCode/palindrome.cpp
+++ LeetCode/palindrome.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0||(x%10==0&&x!=0))
+        return false; 
+        int n=x;
+        long long  rev=0;
+        while(x>0)
+        {
+        int rem=x%10;
+        rev=rev*10+rem;
+        x=x/10;
+        }
+        return rev==n;
+    }
+};
